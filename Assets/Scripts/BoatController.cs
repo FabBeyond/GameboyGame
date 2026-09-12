55958af0c907e2e66745609861afea70a99504e9
FabBeyond <redcraft42.12@gmail.com>
2026-09-12 16:07:27 +0200

    Add inputactions

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -2,5 +2,22 @@ using UnityEngine;
 
 public class BoatController : MonoBehaviour
 {
-    
+    public InputActions input;
+    public float speed;
+
+    private void OnEnable()
+    {
+        input = new InputActions();
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+
+    private void Update()
+    {
+        Vector2 movementInput = input.Player.Movement.ReadValue<Vector2>().normalized * speed * Time.deltaTime;
+        transform.position += new Vector3(movementInput.x, movementInput.y, 0);
+    }
 }
